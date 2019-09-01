ba3147b07b3fffc1b4de8e0f950d649c811aaab8
fivemz <593493999@qq.com>
2019-09-01 12:48:30 +0800

    [Modify] Conflict test.

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,8 +3,8 @@ package hello.world;
 public class HelloWorld {
 	
 	public static void main(String[] args) {		
-		System.out.println("Hello World");
-		System.out.println("Hello Git");
+		System.out.println("Hello master");
+		System.out.println("Hello Git, 23333");
 	}
 
 }
