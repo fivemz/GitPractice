4c9d6ca7459a4668de0f1077960f03b9df4578e9
fivemz <593493999@qq.com>
2019-09-01 12:21:50 +0800

    [Add] Bug.java

---
+++ Bug.java
@@ -0,0 +1,16 @@
+package hello.world;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {		
+		test();
+	}
+	
+	private static void test() {
+		int a = 1;
+		int b = 0;
+		int result = a / b;
+		System.out.println("result = " + result);
+	}
+
+}
